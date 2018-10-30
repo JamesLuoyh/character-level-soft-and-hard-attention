ad8eb93aa6c278cc9df0616a8684bb7da28b8cc6
JamesLuoyh <luoy39@cs.washington.edu>
2018-10-30 08:05:39 -0700

    model done

--- decoder.py
+++ decoder.py
@@ -20,5 +20,47 @@ class Decoder(nn.Module):
         self.pre_output_layer = nn.Linear(hidden_size + 2 * hidden_size + emb_size,
                                           hidden_size, bias=False)
 
-    def forward_step(self, prev_embed, encoder_hidden, src_mask, proj_key, hidden):
-        query = hidden[-1]
+    def forward_step(self, prev_embed, encoder_hidden, src_mask, hidden):
+        query = hidden[-1].unsqueeze(1)
+        context, atten_probs = self.attention(query,
+            value=encoder_hidden, mask=src_mask)
+
+        # input feeding
+        rnn_input = torch.cat([prev_embed, context], dim=2)
+        output, hidden = self.rnn(rnn_input, hidden)
+
+        pre_output = torch.cat([prev_embed, output, context], dim=2) 
+        pre_output = self.dropout_layer(pre_output)
+        pre_output = self.pre_output_layer(pre_output)
+
+        return output, hidden, pre_output
+
+    def forward(self, trg_embed, encoder_hidden, encoder_final,
+                src_mask, trg_mask, hidden=None, max_len=None):
+        
+        if max_len is None:
+            max_len = trg_mask.size(-1)
+
+        if hidden is None:
+            hidden = self.init_hidden(encoder_final)
+        
+        decoder_states = []
+        pre_output_vectors = []
+
+        for i in range(max_len):
+            prev_embed = trg_embed[:, i].unsqueeze(1)
+            output, hidden, pre_output = self.forward_step(
+                prev_embed, encoder_hidden, src_mask, hidden
+            )
+            decoder_states.append(output)
+            pre_output_vectors.append(pre_output)
+
+        decoder_states = torch.cat(decoder_states, dim=1)
+        pre_output_vectors = torch.cat(pre_output_vectors, dim=1)
+        return decoder_states, hidden, pre_output_vectors
+
+    def init_hidden(self, encoder_final):
+        if encoder_final is None:
+            return None
+        
+        return torch.tanh(self.bride(encoder_final))
---
+++ soft_attention.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+class SoftAttention(nn.Module):
+
+    def __init__(self, hidden_size, encoder_size=None, query_size=None):
+        super(SoftAttention, self).__init__()
+
+        # assume bidirectional
+        encoder_size = 2 * hidden_size if encoder_size is None else encoder_size
+        query_size = hidden_size if query_size is None else query_size
+        self.encoder_layer = nn.Linear(encoder_size, hidden_size, bias=False)
+        self.query_layer = nn.Linear(query_size, hidden_size, bias=False)
+        self.attention_layer = nn.Bilinear(hidden_size, hidden_size, 1, bias=False)
+
+    def forward(self, query=None, value=None, mask=None):
+        assert mask is not None, "mask is required"
+        encoder_proj = self.encoder_layer(value)
+        query_proj = self.query_layer(query)
+
+        # Calculate scores.
+        scores = self.attention_layer(encoder_proj, query_proj)
+        scores = scores.squeeze(2).unsqueeze(1)
+        
+        # Mask out invalid positions.
+        # The mask marks valid positions so we invert it using `mask & 0`.
+        scores.data.masked_fill_(mask == 0, -float('inf'))
+        
+        # Turn scores to probabilities.
+        alphas = F.softmax(scores, dim=-1)
+        self.alphas = alphas        
+        
+        # The context vector is the weighted sum of the values.
+        context = torch.bmm(alphas, value)
